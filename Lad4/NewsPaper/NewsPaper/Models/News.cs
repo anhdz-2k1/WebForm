cd49ad1f6eb5b73a4df4b0739d826867d289efd0
Anh Nguyen <anhmanchester2k1@gmail.com>
2020-10-05 12:33:39 +0700

    Lad04

---
+++ Lad4/NewsPaper/NewsPaper/Models/News.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsPaper.Models
+{
+    public partial class News
+    {
+        public News()
+        {
+            Post = new HashSet<Post>();
+        }
+
+        public int NewsId { get; set; }
+        public string Sections { get; set; }
+
+        public virtual ICollection<Post> Post { get; set; }
+    }
+}
---
+++ Lad4/NewsPaper/NewsPaper/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsPaper.Models
+{
+    public partial class Post
+    {
+        public int PostId { get; set; }
+        public int NewsId { get; set; }
+        public string Conten { get; set; }
+        public string Title { get; set; }
+
+        public virtual News News { get; set; }
+    }
+}
