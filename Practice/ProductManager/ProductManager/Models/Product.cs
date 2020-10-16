6bd5c3d2b8a0eb8c62d010767b95f068cc93b7c1
Anh Nguyen <anhmanchester2k1@gmail.com>
2020-10-16 10:55:37 +0700

    Practice

---
+++ Practice/ProductManager/ProductManager/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ProductManager.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+    }
+}
